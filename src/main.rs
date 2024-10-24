3204c7a2271871ee33ea20f6c228a151b60ec000
kyunghyunHan <ekfqlwcjswl@naver.com>
2024-10-24 14:56:49 +0900

    feat:chat

--- src/main.rs
+++ src/main.rs
@@ -3,25 +3,31 @@
 use chrono::{DateTime, NaiveDateTime, TimeZone, Utc};
 use serde::Deserialize;
 use std::fs;
+use yahoo_finance_api as yahoo;
+use rayon::prelude::*; // rayon을 가져옵니다.
+
+use yahoo_finance_api::time::{macros::datetime, OffsetDateTime};
+use yahoo_finance_api::Quote;
 
 use eframe::egui::{self, Color32, Stroke};
 use std::net::UdpSocket; // `egui_plot` crate를 import
-
+use tokio_test;
+use egui_plot::Bar;
 //lib
 use stocki::plot::candle;
-#[derive(Deserialize, Debug)]
-struct Candle {
-    #[serde(rename = "candle_date_time_utc")]
-    candle_date_time_utc: String,
-    #[serde(rename = "opening_price")]
-    opening_price: f64,
-    #[serde(rename = "high_price")]
-    high_price: f64,
-    #[serde(rename = "low_price")]
-    low_price: f64,
-    #[serde(rename = "trade_price")]
-    trade_price: f64,
-}
+// #[derive(Deserialize, Debug)]
+// struct Candle {
+//     #[serde(rename = "candle_date_time_utc")]
+//     candle_date_time_utc: String,
+//     #[serde(rename = "opening_price")]
+//     opening_price: f64,
+//     #[serde(rename = "high_price")]
+//     high_price: f64,
+//     #[serde(rename = "low_price")]
+//     low_price: f64,
+//     #[serde(rename = "trade_price")]
+//     trade_price: f64,
+// }
 fn main() -> eframe::Result {
     env_logger::init(); // Log to stderr (if you run with `RUST_LOG=debug`).
     let options = eframe::NativeOptions {
@@ -46,21 +52,69 @@ struct MyApp {
     socket: UdpSocket,
     destination: String,
     current_screen: Screen,
-    stock_chart: StockChart, // StockChart 필드 추가
     files: Vec<String>,
+    button_text: String,       // 초기 텍스트
+    stocks: Vec<&'static str>, // 주식 이름을 저장할 벡터
+    stock_data: Vec<Bar>,
+}
+pub  fn fetch_quotes() -> Vec<Bar> {
+    let provider = yahoo::YahooConnector::new().unwrap();
+    let start = datetime!(2023-12-20 0:00:00.00 UTC);
+    let end = datetime!(2024-1-1 23:59:59.99 UTC);
+
+    let resp = tokio_test::block_on(provider.get_latest_quotes("AAPL", "1d")).unwrap();
+    let quotes = resp.quotes().unwrap();
+    let red = Color32::from_rgb(255, 0, 0);
+    let green = Color32::from_rgb(0, 255, 0);
+
+    quotes.into_par_iter().map(|quote| {
+        let open = quote.open;
+        let close = quote.close;
+        let volume = quote.volume;
+        let color = if close >= open { green } else { red };
+        let timestamp = quote.timestamp as f64;
+
+        Bar::from(Bar {
+            name: "a".to_string(),
+            orientation: egui_plot::Orientation::Vertical,
+            argument: timestamp,
+            value: volume as f64,
+            base_offset: Some(0.),
+            bar_width: 20.,
+            stroke: Stroke::new(1.0, color),
+            fill: color,
+        })
+    }).collect()
 }
 
+//  fn get_data() -> Vec<Quote> {
+//     let provider = yahoo::YahooConnector::new().unwrap();
+//     let start = datetime!(2023-12-20 0:00:00.00 UTC);
+//     let end = datetime!(2024-1-1 23:59:59.99 UTC);
+//     let resp = tokio_test::block_on(provider.get_quote_history("AAPL", start, end)).unwrap();
+
+//     // let resp = provider
+//     //     .get_quote_history("AAPL", start, end)
+//     //     .await
+//     //     .unwrap();
+//     let quotes = resp.quotes().unwrap();
+//     quotes
+// }
 impl Default for MyApp {
     fn default() -> Self {
         let socket = UdpSocket::bind("127.0.0.1:34254").expect("Couldn't bind to address");
+
         Self {
             name: "Arthur".to_owned(),
             age: 42,
             socket,
             destination: "127.0.0.1:8080".to_owned(),
             current_screen: Screen::Main,
-            stock_chart: StockChart::default(), // StockChart 초기화
-            files: Vec::new(),                  // 빈 벡터로 초기화
+            // stock_chart: StockChart::default(), // StockChart 초기화
+            files: Vec::new(),
+            button_text: "Stocks".to_string(), // 초기 텍스트
+            stocks: vec!["GOOG", "GOOG2"],     // 초기 주식 이름
+            stock_data: fetch_quotes(),            // 빈 벡터로 초기화
         }
     }
 }
@@ -107,7 +161,7 @@ impl eframe::App for MyApp {
             });
         });
         egui::Window::new("Stock Picker").show(ctx, |ui| {
-            println!("{}","힝");
+            println!("{}", "힝");
             ui.horizontal_wrapped(|ui| {
                 ui.spacing_mut().text_edit_width = 50.;
                 ui.label("Stock:");
@@ -115,16 +169,16 @@ impl eframe::App for MyApp {
             });
             ui.horizontal(|ui| {
                 if ui.button("PICK").clicked() {
-                    // self.stocks_map.lock().unwrap().insert(self.stock.clone(), 
+                    // self.stocks_map.lock().unwrap().insert(self.stock.clone(),
                     //     Arc::new(Mutex::new(Stock::default(&self.stock))));
                 }
             });
         });
         egui::CentralPanel::default().show(ctx, |ui| {
-            ui.horizontal(|ui| {
-                ui.heading("Main Screen");
-            });
-            ui.separator();
+            // ui.horizontal(|ui| {
+            //     ui.heading("Main Screen");
+            // });
+            // ui.separator();
 
             ui.horizontal(|ui| {
                 // 사이드바를 포함하는 좌측 영역
@@ -145,7 +199,7 @@ impl eframe::App for MyApp {
                 // 메인 화면 내용 표시
                 ui.vertical(|ui| match self.current_screen {
                     Screen::Main => {
-                        ui.heading("Main Screen");
+                        // ui.heading("Main Screen");
                         ui.horizontal(|ui| {
                             let name_label = ui.label("Your name: ");
                             ui.text_edit_singleline(&mut self.name)
@@ -188,10 +242,28 @@ impl eframe::App for MyApp {
                     }
 
                     Screen::Tertiary => {
-                        ui.heading("Stock Price Chart");
+                        // ui.heading("Stock Price Chart");
+
+                        ui.group(|ui| {
+                            // ui.label("Candle Chart Section");
+                            // candle::candle_chart(ui);
+                            let button_text = self.button_text.clone(); // 불변 참조의 복사본 생성
+
+                            ui.menu_button(&button_text, |ui| {
+                                for &stock in &self.stocks {
+                                    if ui.button(stock).clicked() {
+                                        println!("{} 클릭", stock);
+                                        self.button_text = stock.to_string(); // 가변 참조를 수정
+                                        ui.close_menu(); // 메뉴 닫기
+                                    }
+                                }
+                            });
+                        });
+                        // candle::candle_chart(ui);
+
                         ui.group(|ui| {
                             ui.label("Candle Chart Section");
-                            candle::candle_chart(ui);
+                            candle::candle_chart(ui,&self.stock_data);
                         });
                     }
                 });
--- src/plot/candle.rs
+++ src/plot/candle.rs
@@ -8,46 +8,9 @@ use rayon::prelude::*; // rayon을 가져옵니다.
 
 use reqwest::{header, Error};
 use serde::Deserialize;
-pub fn candle_chart(ui: &mut egui::Ui) {
-    println!("호출대나");
-    let provider = yahoo::YahooConnector::new().unwrap();
-    let start = datetime!(1990-1-1 0:00:00.00 UTC);
-    let end = datetime!(2024-1-1 23:59:59.99 UTC);
-    let resp = tokio_test::block_on(provider.get_quote_history("AAPL", start, end)).unwrap();
-    let quotes = resp.quotes().unwrap();
-    let red = Color32::from_rgb(255, 0, 0);
-    let green = Color32::from_rgb(0, 255, 0);
 
-    // println!("주식 갯수:{}", quotes.len());
-
-    let bars: Vec<Bar> = quotes.par_iter().map(|quote| {
-        let open = quote.open; // 개장가
-        let high = quote.high; // 고가
-        let low = quote.low; // 저가
-        let close = quote.close; // 종가
-        let volume = quote.volume;
-        // 종가와 개장가를 비교하여 색상을 결정
-        let color = if close >= open {
-            green // 종가가 개장가보다 높으면 녹색
-        } else {
-            red   // 종가가 개장가보다 낮으면 빨간색
-        };
-        let timestamp = quote.timestamp as f64;
-
-        let bar = Bar::from(Bar {
-            name: "a".to_string(),
-            orientation: Orientation::Vertical, // 세로 막대그래프
-            argument: timestamp,
-            value: volume as f64,           // Y축 값 (종가를 막대 높이로 사용)
-            base_offset: Some(0.), // 막대가 시작하는 기준 (저가를 기준으로 할 수 있음)
-            bar_width: 20.,
-            stroke: Stroke::new(1.0, color),
-            fill: color,
-        });
-
-        bar
-    }).collect(); // 바를 수집합니다.
-    let data = BarChart::new(bars);
+pub fn candle_chart(ui: &mut egui::Ui,bars:& Vec<Bar>) {
+    let data = BarChart::new(bars.clone());
     let plot = Plot::new("candlestick chart").view_aspect(10.0);
     plot.show(ui, |plot_ui| {
         plot_ui.bar_chart(data);
